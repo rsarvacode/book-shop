d97cdad1bae9e3d263ccf753c6798de571a34c43
Robertas <Robertas.sarva@gmail.com>
2021-02-09 22:45:50 +0200

    Added Models

--- app/Models/author.php
+++ app/Models/author.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use \DateTimeInterface;
 
-class author extends Model
+class Author extends Model
 {
-    use HasFactory;
+    use SoftDeletes, HasFactory;
+
+    public $table = 'authors';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
